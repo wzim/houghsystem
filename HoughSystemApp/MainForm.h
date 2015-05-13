c290b03bc5accc9db114d347c923f0f8ede05cb7
marcin.olejniczak <marcin.olejniczak@zilp.lasy.gov.pl>
2015-05-13 10:49:44 +0200

    Aktualizacja formatki głównej - dodanie menu głównego

--- HoughSystemApp/MainForm.h
+++ HoughSystemApp/MainForm.h
@@ -34,6 +34,10 @@ namespace HoughSystemApp {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::MenuStrip^  menuStrip1;
+	protected: 
+	private: System::Windows::Forms::ToolStripMenuItem^  systemToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^  zamknijToolStripMenuItem;
 
 	private:
 		/// <summary>
@@ -48,18 +52,53 @@ namespace HoughSystemApp {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->menuStrip1 = (gcnew System::Windows::Forms::MenuStrip());
+			this->systemToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->zamknijToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->menuStrip1->SuspendLayout();
 			this->SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this->menuStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(1) {this->systemToolStripMenuItem});
+			this->menuStrip1->Location = System::Drawing::Point(0, 0);
+			this->menuStrip1->Name = L"menuStrip1";
+			this->menuStrip1->Size = System::Drawing::Size(930, 24);
+			this->menuStrip1->TabIndex = 0;
+			this->menuStrip1->Text = L"menuStrip1";
+			// 
+			// systemToolStripMenuItem
+			// 
+			this->systemToolStripMenuItem->DropDownItems->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(1) {this->zamknijToolStripMenuItem});
+			this->systemToolStripMenuItem->Name = L"systemToolStripMenuItem";
+			this->systemToolStripMenuItem->Size = System::Drawing::Size(57, 20);
+			this->systemToolStripMenuItem->Text = L"System";
+			// 
+			// zamknijToolStripMenuItem
+			// 
+			this->zamknijToolStripMenuItem->Name = L"zamknijToolStripMenuItem";
+			this->zamknijToolStripMenuItem->Size = System::Drawing::Size(152, 22);
+			this->zamknijToolStripMenuItem->Text = L"Zamknij";
+			this->zamknijToolStripMenuItem->Click += gcnew System::EventHandler(this, &MainForm::zamknijToolStripMenuItem_Click);
 			// 
 			// MainForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(930, 511);
+			this->Controls->Add(this->menuStrip1);
+			this->MainMenuStrip = this->menuStrip1;
 			this->Name = L"MainForm";
 			this->Text = L"MainForm";
+			this->menuStrip1->ResumeLayout(false);
+			this->menuStrip1->PerformLayout();
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
+	private: System::Void zamknijToolStripMenuItem_Click(System::Object^  sender, System::EventArgs^  e) {
+				 this->Close();
+			 }
 	};
 }
