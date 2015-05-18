40d2e8489eb8e8c666d37d91442d33d544fa968a
marcin.olejniczak <marcin.olejniczak@zilp.lasy.gov.pl>
2015-05-18 12:27:44 +0200

    Aktualizacja ścieżek do projektu

--- HoughSystemApp/MainForm.h
+++ HoughSystemApp/MainForm.h
@@ -41,6 +41,7 @@ namespace HoughSystemApp {
 	private: System::Windows::Forms::ToolStripMenuItem^  zamknijToolStripMenuItem;
 	private: System::Windows::Forms::ToolStripMenuItem^  algorytmyToolStripMenuItem;
 	private: System::Windows::Forms::ToolStripMenuItem^  cannyToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^  bigMatrixToolStripMenuItem;
 
 	private:
 		/// <summary>
@@ -60,6 +61,7 @@ namespace HoughSystemApp {
 			this->zamknijToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
 			this->algorytmyToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
 			this->cannyToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->bigMatrixToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
 			this->menuStrip1->SuspendLayout();
 			this->SuspendLayout();
 			// 
@@ -89,7 +91,8 @@ namespace HoughSystemApp {
 			// 
 			// algorytmyToolStripMenuItem
 			// 
-			this->algorytmyToolStripMenuItem->DropDownItems->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(1) {this->cannyToolStripMenuItem});
+			this->algorytmyToolStripMenuItem->DropDownItems->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(2) {this->cannyToolStripMenuItem, 
+				this->bigMatrixToolStripMenuItem});
 			this->algorytmyToolStripMenuItem->Name = L"algorytmyToolStripMenuItem";
 			this->algorytmyToolStripMenuItem->Size = System::Drawing::Size(75, 20);
 			this->algorytmyToolStripMenuItem->Text = L"Algorytmy";
@@ -100,6 +103,13 @@ namespace HoughSystemApp {
 			this->cannyToolStripMenuItem->Size = System::Drawing::Size(152, 22);
 			this->cannyToolStripMenuItem->Text = L"Canny";
 			this->cannyToolStripMenuItem->Click += gcnew System::EventHandler(this, &MainForm::cannyToolStripMenuItem_Click);
+			// 
+			// bigMatrixToolStripMenuItem
+			// 
+			this->bigMatrixToolStripMenuItem->Name = L"bigMatrixToolStripMenuItem";
+			this->bigMatrixToolStripMenuItem->Size = System::Drawing::Size(152, 22);
+			this->bigMatrixToolStripMenuItem->Text = L"Big Matrix";
+			this->bigMatrixToolStripMenuItem->Click += gcnew System::EventHandler(this, &MainForm::bigMatrixToolStripMenuItem_Click);
 			// 
 			// MainForm
 			// 
@@ -125,5 +135,15 @@ namespace HoughSystemApp {
 				 canny.setSource("C:\\Users\\marcin.olejniczak\\Pictures\\zwierzaki\\deer_1.jpg");
 				 canny.CannyThreshold(0, 0);
 			 }
+private: System::Void bigMatrixToolStripMenuItem_Click(System::Object^  sender, System::EventArgs^  e) {
+			 std::string nazwa_okna = "Matrix"; 
+			 cv::namedWindow(nazwa_okna, WINDOW_NORMAL);
+			 cv::Mat m(70000, 10000, CV_8UC1);
+			 m.setTo(cv::Scalar(5, 10, 15));
+			 cv::Vec3b color = m.at<cv::Vec3b>(cv::Point(100,100));
+			 color[2] = 255; color[1] = 255; color[0] = 255;
+			 m.at<cv::Vec3b>(cv::Point(100,100)) = color;
+			 cv::imshow(nazwa_okna, m);
+		 }
 };
 }
